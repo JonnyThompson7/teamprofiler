8957d1fbe03858bab5fab349896ef8e6f1f7df3c
Jonathan Thompson <jathompson399@gmail.com>
2022-08-08 18:42:22 -0500

    first tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('John', 'johndoe@asdf.com', '0');
+
+    expect(employee.name).toBe('John');
+    expect(employee.email).toBe('johndoe@asdf.com');
+    expect(employee.id).toBe('0'); 
+});
